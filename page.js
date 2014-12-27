153cf0b0fcbf59ad9b792fa5dcb252e52fef5cc0
Alexander01998 <contact.wurstimperium@gmail.com>
2014-12-27 15:06:11 +0100

    -

--- page.js
+++ page.js
@@ -1,4 +1,4 @@
 ﻿$(document).ready(function(){
-	$("header").load("page_header.html");
-	$("footer").load("page_footer.html");
+	$("header").load("/page_header.html");
+	$("footer").load("/page_footer.html");
 });
