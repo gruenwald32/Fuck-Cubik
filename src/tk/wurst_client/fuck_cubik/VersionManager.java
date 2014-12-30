982732208886d9bfdd3dc3bb06d2c343caa8917e
Alexander01998 <contact.wurstimperium@gmail.com>
2014-12-30 23:09:41 +0100

    -

--- src/tk/wurst_client/fuck_cubik/VersionManager.java
+++ src/tk/wurst_client/fuck_cubik/VersionManager.java
@@ -4,9 +4,9 @@ package tk.wurst_client.fuck_cubik;
 public class VersionManager
 {
 	public static final byte MAJOR_VERSION = 1;
-	public static final byte MINOR_VERSION = 2;
-	public static final byte PATCH = 1;
-	public static final long BUILD = 48;
+	public static final byte MINOR_VERSION = 3;
+	public static final byte PATCH = 0;
+	public static final long BUILD = 49;
 	public static final byte PRE_RELEASE = 0;
 	
 	public static final String SIMPLE_VERSION = MAJOR_VERSION
