a68633dd55742179de0c90f66788ed9417066c9a
Alexander01998 <contact.wurstimperium@gmail.com>
2015-01-04 20:41:02 +0100

    Attempt 2 to fix smooth scrolling

--- templates/modern/scrolling.js
+++ templates/modern/scrolling.js
@@ -1,26 +1,30 @@
-﻿$(document).ready(function(){
-	$(window).scroll(function() {
-		if ($("header").offset().top > 50) {
+﻿$(document).ready(function()
+{
+	$(window).scroll(function()
+	{
+		if ($("header").offset().top > 50)
+		{
 			$("header").addClass("collapse");
-		} else {
+		}else
+		{
 			$("header").removeClass("collapse");
 		}
 	});
 	
-	$(function() {
-	  $('a[href*=#]').click(function() {
-	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-
-	      var target = $(this.hash);
-	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	      if (target.length) {
-	        $('html,body').animate({
-	          scrollTop: target.offset().top
-	        }, 1500);
-	        return false;
-	      }
-	    }
-	  });
+	$(function()
+	{
+		$('a[href*=#]').click(function()
+		{
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname)
+			{
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				$('html,body').animate(
+				{
+					scrollTop: target.offset().top
+				}, 1500);
+				return false;
+			}
+		});
 	});
-	
 });
