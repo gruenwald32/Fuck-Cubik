b6f737446f383d9e3bf316ce491b39ca60147b65
Alexander01998 <contact.wurstimperium@gmail.com>
2015-01-04 20:30:52 +0100

    Attempt to fix smooth scrolling to the top

--- templates/modern/scrolling.js
+++ templates/modern/scrolling.js
@@ -8,7 +8,7 @@
 	});
 	
 	$(function() {
-	  $('a[href*=#]:not([href=#])').click(function() {
+	  $('a[href*=#]').click(function() {
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 
 	      var target = $(this.hash);
