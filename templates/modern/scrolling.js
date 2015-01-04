630f59906189ac3a67d51dc422ff61e3a563adf5
Alexander01998 <contact.wurstimperium@gmail.com>
2015-01-04 14:27:55 +0100

    -

---
+++ templates/modern/scrolling.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function(){
+	$(window).scroll(function() {
+		if ($("header").offset().top > 50) {
+			$("header").addClass("collapse");
+		} else {
+			$("header").removeClass("collapse");
+		}
+	});
+	
+	$(function() {
+		$('a.scroll').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+});
